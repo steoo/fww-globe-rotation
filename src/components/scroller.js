394a04297d5b4e1f602e3549809974ef2cbae646
stefano imparato <esseiproject@gmail.com>
2024-02-23 20:07:54 +0100

    missing files

---
+++ src/components/scroller.js
@@ -0,0 +1,47 @@
+export default function scrollerEnabler(controls) {
+  // Assuming `controls` is your instance of OrbitControls
+  let touchStartX = 0;
+  let touchStartY = 0;
+  let touchEndY = 0;
+
+  // Function to enable/disable OrbitControls
+  function setOrbitControlsEnabled(enabled) {
+    controls.enabled = enabled;
+    enabled
+      ? document.querySelector("#globeSection").style.zIndex = 0
+      : document.querySelector("#globeSection").style.zIndex = -1
+  }
+
+  // Listen for touchstart event to capture the start position
+  document.addEventListener('touchstart', function(e) {
+    touchStartX = e.touches[0].clientX;
+    touchStartY = e.touches[0].clientY;
+    setOrbitControlsEnabled(true);
+  }, false);
+
+  // Listen for touchmove event to determine the direction
+  document.addEventListener('touchmove', function(e) {
+    touchEndY = e.touches[0].clientY;
+    
+    // Calculate the difference in touch start and end Y positions
+    var deltaY = touchEndY - touchStartY;
+    
+    // A threshold to determine a "vertical swipe" can be adjusted as needed
+    var verticalSwipeThreshold = 10;
+
+    // Check if the vertical movement is significant enough to be considered a swipe
+    if (Math.abs(deltaY) > verticalSwipeThreshold) {
+      // Disable orbit controls for vertical swipe
+      setOrbitControlsEnabled(false);
+    } else {
+      // Enable orbit controls for horizontal movement or minor vertical movements
+      setOrbitControlsEnabled(true);
+    }
+  }, false);
+
+  // Optionally, re-enable controls when the touch ends
+  document.addEventListener('touchend', function (e) {
+    // setOrbitControlsEnabled(false);
+  }, false);
+
+}
